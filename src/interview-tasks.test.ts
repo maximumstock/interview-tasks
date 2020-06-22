b43def7d2bfcfe00c817cb87464afc6cab51d472
BogdanSinitsa <BogdanSinitsa@gmail.com>
2020-06-22 09:26:08 +0300

    Update interview-tasks.test.ts

--- src/interview-tasks.test.ts
+++ src/interview-tasks.test.ts
@@ -6,7 +6,7 @@ test('sort', () => {
     expect(interviewTasks.sort([5,3,1,2,6,7,4])).toEqual([1,2,3,4,5,6,7]);
 });
 
-test('sort', () => {
+test('fibonacci', () => {
     expect(interviewTasks.fibonacci(7)).toEqual([1, 1, 2, 3, 5, 8, 13]);
 });
 
