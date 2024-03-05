be84bbb1f41617ff0da5e5a3467f6036173eec31
Maximilian Stock <maximilian.stock@veeva.com>
2024-03-05 10:13:10 +0100

    chore: clean-up

--- src/interview-tasks.test.ts
+++ src/interview-tasks.test.ts
@@ -1,19 +1,15 @@
-import InterviewTasks from './interview-tasks';
+import InterviewTasks from "./interview-tasks";
 
 const interviewTasks = new InterviewTasks();
 
-test('sort', () => {
-    expect(interviewTasks.sort([5,3,1,2,6,7,4])).toEqual([1,2,3,4,5,6,7]);
+test("fibonacci", () => {
+  expect(interviewTasks.fibonacci(7)).toEqual([1, 1, 2, 3, 5, 8, 13]);
 });
 
-test('fibonacci', () => {
-    expect(interviewTasks.fibonacci(7)).toEqual([1, 1, 2, 3, 5, 8, 13]);
-});
-
-test('anagram', () => {
-    expect(interviewTasks.anagram('fried', 'fired')).toBeTruthy();
-    expect(interviewTasks.anagram('gainly', 'laying')).toBeTruthy();
-    expect(interviewTasks.anagram('sadder', 'dreads')).toBeTruthy();
-    expect(interviewTasks.anagram('listen', 'silent')).toBeTruthy();
-    expect(interviewTasks.anagram('orange', 'apple')).toBeFalsy();
+test("anagram", () => {
+  expect(interviewTasks.anagram("fried", "fired")).toBeTruthy();
+  expect(interviewTasks.anagram("gainly", "laying")).toBeTruthy();
+  expect(interviewTasks.anagram("sadder", "dreads")).toBeTruthy();
+  expect(interviewTasks.anagram("listen", "silent")).toBeTruthy();
+  expect(interviewTasks.anagram("orange", "apple")).toBeFalsy();
 });
--- src/interview-tasks.ts
+++ src/interview-tasks.ts
@@ -1,21 +1,14 @@
 export default class InterviewTasks {
+  //input: 7
+  //output: [1, 1, 2, 3, 5, 8, 13]
+  fibonacci(n: number): number[] {
+    return [];
+  }
 
-    //input: [5,3,1,2,6,7,4]
-    //output: [1,2,3,4,5,6,7]
-    sort(values: number[]): number[] {
-        return []
-    }
-
-    //input: 7
-    //output: [1, 1, 2, 3, 5, 8, 13]
-    fibonacci(n: number): number[] {
-        return [];
-    }
-
-    // A word is an anagram of another word if both use the same letters in the same quantity,
-    // but arranged differently.
-    // input: a = 'fried' b = 'fired'
-    anagram(a: string, b: string): boolean {
-        return;
-    }
+  // A word is an anagram of another word if both use the same letters in the same quantity,
+  // but arranged differently.
+  // input: a = 'fried' b = 'fired'
+  anagram(a: string, b: string): boolean {
+    return;
+  }
 }
